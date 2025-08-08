0dd9af1fb5421bd9b6810c7a8477c9e5f4d4b57d
Matias-Artesi <matias.artesi02@gmail.com>
2025-08-08 12:54:53 -0300

    Update __manifest__.py

--- invoice_filename_so_first/__manifest__.py
+++ invoice_filename_so_first/__manifest__.py
@@ -1,13 +1,14 @@
 {
     "name": "Invoice PDF Filename: SO first",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.2",
     "category": "Accounting",
     "summary": "Prefix invoice PDF filename with Sale Order name, then the invoice number.",
     "author": "Matías Artesi",
     "license": "LGPL-3",
     "depends": [
         "account",
-        "sale"
+        "sale",
+        "l10n_ar"
     ],
     "data": [],
     "installable": True,
